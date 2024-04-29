6be282b171aeb178b528b7dcaa04e18c3601a19b
Eldenabih Tavirazin Lutvie <tavirazin@gmail.com>
2024-04-29 08:17:28 +0700

    Initial Commit

---
+++ NC1_TIMCooker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NC1_TIMCooker
+//
+//  Created by Eldenabih Tavirazin Lutvie on 29/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NC1_TIMCooker/NC1_TIMCookerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC1_TIMCookerApp.swift
+//  NC1_TIMCooker
+//
+//  Created by Eldenabih Tavirazin Lutvie on 29/04/24.
+//
+
+import SwiftUI
+
+@main
+struct NC1_TIMCookerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
